ad93dc352826c65794b0e85af7f925de65d47ee0
2320030402 <149470685+2320030402@users.noreply.github.com>
2024-08-22 23:15:08 +0530

    Add files via upload

---
+++ MusicStreamingApp/src/com/music/musicplayer.java
@@ -0,0 +1,16 @@
+package com.music;
+
+
+	public abstract class musicplayer implements musicsrc {
+	    protected musicsrc decoratedMusicSource;
+
+	    public musicplayer(musicsrc decoratedMusicSource) {
+	        this.decoratedMusicSource = decoratedMusicSource;
+	    }
+
+	    public void play() {
+	        decoratedMusicSource.play();
+	    }
+	}
+
+
---
+++ Musicstream/src/com/musicstream/musicd.java
@@ -0,0 +1,15 @@
+package com.musicstream;
+
+
+public abstract class musicd implements musicb {
+    protected musicb decoratedMusicSource;
+
+    public musicd(musicb decoratedMusicSource) {
+        this.decoratedMusicSource = decoratedMusicSource;
+    }
+
+    @Override
+    public void play() {
+        decoratedMusicSource.play();
+    }
+}
